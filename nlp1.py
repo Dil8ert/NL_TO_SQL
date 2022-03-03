e16b286dad9ec1ecc39eb781b51d74ca195127b0
Param Shelar <60221741+Dil8ert@users.noreply.github.com>
2022-03-04 00:20:16 +0530

    Add files via upload

--- nlp1.py
+++ nlp1.py
@@ -1,35 +1,41 @@
 import spacy
-
+import csv
+from spacy.matcher import PhraseMatcher
 # Import the Matcher
-from spacy.matcher import Matcher
 
+file = open(r'C:\Users\Shela\Downloads\All_Indian_Trains.csv')
+csvreader = csv.reader(file)
+header = next(csvreader)
+starts = []
+for i in csvreader:
+    starts.append(i[3])
+# print(starts)
 # Load a pipeline and create the nlp object
-nlp = spacy.load("en_core_web_sm")
 def NLP_SQL(text):
+
+    nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
-    # Initialize the matcher with the shared vocab
-    matcher = Matcher(nlp.vocab)
+    matcher = PhraseMatcher(nlp.vocab)
+    patterns = list(set((nlp.pipe(starts))))
 
-    # Add the pattern to the matcher
-    pattern = [{"POS": "PROPN"}]
-    matcher.add("PATTERN_1", [pattern])
+    matcher.add("Cities", patterns)
 
-    # Process some text
 
-    matched = []
 
-    # Call the matcher on the doc
     matches = matcher(doc)
-    for token in doc:
-        if(token.pos_ == 'PROPN'):
-            if(doc[token.i+1].pos_ == 'ADP'):
-                starts_at = token.text
-                ends_at = doc[token.i+2].text
-
-    
+    matched_span = [doc[start:end] for match_id, start, end in matches]
+    starts_at = matched_span[0]
+    ends_at = matched_span[-1]
 
     sql = "SELECT * FROM TRAINS WHERE START = '{}' AND ENDS = '{}'".format(starts_at,ends_at)
     print(sql)
     return sql
 
-# NLP_SQL("List trains available from bombay to goa.")
+
+
+
+
+
+
+
+# NLP_SQL("List trains available from New Delhi to Aurangabad.")
